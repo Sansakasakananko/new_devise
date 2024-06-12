3d4bd6070fa09799c5a29a403ab04b3804695aa9
sansakasakananoko <misaka.seiko@gmail.com>
2024-06-12 17:30:17 +0900

    ポストの編集画面まで終了

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
 class Post < ApplicationRecord
   belongs_to:user
   validates :context, presence: true, length: {maximum: 200}
+  has_one_attached :photo
+
+  def picture(a, b)
+    photo.variant(resize_to_fill:[a, b])
+  end
+
 end
